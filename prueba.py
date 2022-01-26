45e06444fdf140eba1eb3fbe2fef2c8b99d5d67a
Roberto Rangel Heras <mcrangelheras@gmail.com>
2022-01-26 08:01:55 -0600

    Se agrega prueba.cpp

--- prueba.py
+++ prueba.py
@@ -2,11 +2,11 @@ print("Hola mundo")
 edad = int(input("Dame tu edad: "))
 nombre = input("Dame tu nombre: ")
 print("Te llamas: "+nombre)
-print("y tienes "+edad+" años")
+print("y tienes "+str(edad)+" años")
 print("Cualquier cosa")
 print("Cualquier otra cosa")
 
 print('DNS \' " ')
 
-print("Algo más Robert's dog \" ")
+print("Algo más Robert's dog")
 
